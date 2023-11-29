3e6632b8d170c55ed8794f65e8bd529fbbcd116b
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2023-11-29 16:49:25 +0100

    learned about inherited model

--- inherited_model_practice/lib/main.dart
+++ inherited_model_practice/lib/main.dart
@@ -1,125 +1,148 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'dart:developer' as marach show log;
 
-void main() {
-  runApp(const MyApp());
-}
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.blue)
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
-  final String title;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  var color1 = Colors.green, color2 = Colors.blue;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: const Text('InheritedModel Example')),
+      body: AvailableColorsModel(
+        color1: color1, color2: color2, 
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Row(
+              children: [
+                TextButton(
+                  onPressed: (){setState((){color1 = listOfColors.getRadomElement();});},
+                  child: const Text('Change Color1')
+                ),
+                TextButton(
+                  onPressed: (){setState((){color2 = listOfColors.getRadomElement();});},
+                  child: const Text('Change Color2')
+                )
+              ]
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            const ColorWidget(color: AvailableColors.one),
+            const ColorWidget(color: AvailableColors.two)
+          ]
+        )
+      )
     );
   }
+}
+
+
+
+
+
+
+
+final List<MaterialColor> listOfColors = [
+  Colors.blue, Colors.red, Colors.yellow, Colors.orange, 
+  Colors.purple, Colors.cyan, Colors.brown, Colors.amber, Colors.deepPurple
+];
+
+
+
+
+enum AvailableColors{one, two}
+
+
+
+
+class AvailableColorsModel extends InheritedModel<AvailableColors> {
+  final MaterialColor color1, color2;
+  const AvailableColorsModel({
+    Key? key, required this.color1, required Widget child, required this.color2,
+  }): super(key: key, child: child);
+  
+
+  static AvailableColorsModel? of(BuildContext context, AvailableColors colorAspect){
+    return InheritedModel.inheritFrom<AvailableColorsModel>(context, aspect: colorAspect);
+  }
+  @override
+  bool updateShouldNotify(covariant AvailableColorsModel oldWidget) {
+    marach.log('either color1 or color2 has changed, Hence UpdateShouldNotify was called');
+    return color1 != oldWidget.color1 || color2 != oldWidget.color2;
+  }
+  
+  @override
+  bool updateShouldNotifyDependent(covariant AvailableColorsModel oldWidget, Set<AvailableColors> dependencies) {
+    if (dependencies.contains(AvailableColors.one) && color1 != oldWidget.color1) {
+      marach.log('Color1 has changed'); return true;
+    }
+    if (dependencies.contains(AvailableColors.two) && color2 != oldWidget.color2) {
+      marach.log('Color2 has changed'); return true;
+    }
+    return false;
+  }
+
+}
+
+
+
+
+
+
+class ColorWidget extends StatelessWidget {
+  final AvailableColors color;
+  const ColorWidget({required this.color, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    switch(color){
+      case AvailableColors.one:
+        marach.log('Color1 got rebuilt');
+        break;
+      case AvailableColors.two:
+        marach.log('Color2 got rebuilt');
+        break;
+    }
+    final colors = AvailableColorsModel.of(context, color);
+    return Container(
+      height: 100, color: color == AvailableColors.one? colors!.color1: colors!.color2,
+    );
+  }
+}
+
+
+
+
+
+
+extension RandomElement<T> on Iterable<T> {
+  T getRadomElement() => elementAt(Random().nextInt(length));
 }
